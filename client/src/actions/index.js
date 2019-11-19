3abceb7201d41bad8dad5f652f6c6d79a45f7f9e
snavdeepsingh <snavdeepsingh@gmail.com>
2019-11-19 01:14:34 -0600

    Added history.js. Reducer function to create, edit, delete and Fetch streams.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -25,10 +26,14 @@ export const signOut = () => {
     }
 }
 
-export const createStream = formValues =>  async dispatch => {
-      const response = await streams.post('/streams', formValues);
+export const createStream = formValues =>  async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId});
 
-      dispatch({ type: CREATE_STREAM, payload: response.data })
+    dispatch({ type: CREATE_STREAM, payload: response.data })
+
+    // Programmatic navigation to get the user back to the root route
+    history.push('/');
 }
 
 export const fetchStreams = () => async dispatch => {
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { Router, Route} from 'react-router-dom';
 
 import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
@@ -7,6 +7,7 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import history from '../history.js';
 
 
 
@@ -14,16 +15,16 @@ function App() {
   return (
     <div className="ui container">
       
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
